f8d46a3f9980152048fffc2865376c57a5713e13
Eric Hontz <airikh@gmail.com>
2015-12-19 01:36:34 -0500

    Add rest of input layers and define GInsets

--- src/tipcalc.c
+++ src/tipcalc.c
@@ -2,23 +2,33 @@
 #include "calculator.h"
 #include "tipcalc.h"
 
-#define NUM_INPUT_FIELDS 4
+#define BOARDER 3
+#define ROW_1_Y 4
+#define ROW_2_Y 71
+#define ROW_3_Y 139
 #define BUTTON_HOLD_REPEAT_MS 100
+#define NUM_INPUT_FIELDS 4
 
 
 static int *current_input_idx;  // use in click handlers to select callbacks
 static InputField *input_fields[NUM_INPUT_FIELDS];
+
 static Window *main_window;
+
 static Layer *main_layer;
 static Layer *selection_indicator;
 static Layer *bill_dollars_layer;
 static Layer *bill_cents_layer;
 static Layer *tip_percent_layer;
 static Layer *num_splitting_layer;
+
 static GFont helvetica_18;
 static GFont helvetica_22;
 static GFont helvetica_24;
 static GFont helvetica_26;
+
+static GEdgeInsets helvetica_22_insets = {6 - BOARDER, 1 - BOARDER, 1 - BOARDER, 1 - BOARDER};  // t, r, l, b
+static GEdgeInsets helvetica_24_insets = {7 - BOARDER, 1 - BOARDER, 1 - BOARDER, 1 - BOARDER};
 
 
 static void input_field_update_proc(Layer *layer, GContext *ctx) {
@@ -29,7 +39,9 @@ static void input_field_update_proc(Layer *layer, GContext *ctx) {
     graphics_context_set_text_color(ctx, GColorWhite);
     // Draw the selection box.
     graphics_context_set_fill_color(ctx, GColorBlack);
-    graphics_fill_rect(ctx, input_field->selection_frame, 0, GCornerNone);
+    // TODO: implement dynamic selection frame sizing based on text size?
+    GRect selection_frame = grect_inset(input_field->text_frame, input_field->selection_insets);
+    graphics_fill_rect(ctx, selection_frame, 0, GCornerNone);
   } else {
     // Set the text color to white.
     graphics_context_set_text_color(ctx, GColorBlack);
@@ -80,37 +92,82 @@ static void click_config_provider(void *context) {
 
 static void main_window_load(Window* window) {
   main_layer = window_get_root_layer(main_window);
-
+  helvetica_18 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_HELVETICA_ROUNDED_18));
+  helvetica_22 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_HELVETICA_ROUNDED_22));
   helvetica_24 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_HELVETICA_ROUNDED_24));
+  helvetica_26 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_HELVETICA_ROUNDED_26));
 
   InputField *input_field;
   // Bill dollars
   bill_dollars_layer = input_layer_create();
- input_field = layer_get_data(bill_dollars_layer);
+  input_field = layer_get_data(bill_dollars_layer);
   *input_field = (InputField) {
       .font = helvetica_24,
-      .text_frame = GRect(61, 12, 48, 25),
-      .selection_frame = GRect(58, 15, 50, 24),
+      .text_frame = GRect(46, ROW_1_Y, 48, 25),
+      .selection_insets = helvetica_24_insets,
       .get_text = calc_get_bill_dollars_txt,
       .inc_value = calc_inc_bill_dollars,
       .dec_value = calc_dec_bill_dollars,
       .is_selected = true
   };
-  layer_mark_dirty(bill_dollars_layer);
+  input_fields[0] = input_field;
+  // Bill cents
+  bill_cents_layer = input_layer_create();
+  input_field = layer_get_data(bill_cents_layer);
+  *input_field = (InputField) {
+      .font = helvetica_24,
+      .text_frame = GRect(108, ROW_1_Y, 32, 25),
+      .selection_insets = helvetica_24_insets,
+      .get_text = calc_get_bill_cents_txt,
+      .inc_value = calc_inc_bill_cents,
+      .dec_value = calc_dec_bill_cents,
+      .is_selected = true
+  };
+  input_fields[1] = input_field;
+  // Tip percent
+  tip_percent_layer = input_layer_create();
+  input_field = layer_get_data(tip_percent_layer);
+  *input_field = (InputField) {
+      .font = helvetica_22,
+      .text_frame = GRect(4, ROW_2_Y, 30, 23),
+      .selection_insets = helvetica_22_insets,
+      .get_text = calc_get_tip_percent_txt,
+      .inc_value = calc_inc_tip_percent,
+      .dec_value = calc_dec_tip_percent,
+      .is_selected = true
+  };
+  input_fields[2] = input_field;
+  // Num splitting
+  num_splitting_layer = input_layer_create();
+  input_field = layer_get_data(num_splitting_layer);
+  *input_field = (InputField) {
+      .font = helvetica_22,
+      .text_frame = GRect(4, ROW_3_Y, 15, 23),
+      .selection_insets = helvetica_22_insets,
+      .get_text = calc_get_num_splitting_txt,
+      .inc_value = calc_inc_num_splitting,
+      .dec_value = calc_dec_num_splitting,
+      .is_selected = true
+  };
+  input_fields[3] = input_field;
+
+
+  layer_mark_dirty(main_layer);
 }
 
 
 static void main_window_unload(Window *window) {
-  fonts_unload_custom_font(helvetica_18);
-  fonts_unload_custom_font(helvetica_22);
-  fonts_unload_custom_font(helvetica_24);
-  fonts_unload_custom_font(helvetica_26);
-
   layer_destroy(selection_indicator);
   layer_destroy(bill_dollars_layer);
   layer_destroy(bill_cents_layer);
   layer_destroy(tip_percent_layer);
   layer_destroy(num_splitting_layer);
+
+  fonts_unload_custom_font(helvetica_18);
+  fonts_unload_custom_font(helvetica_22);
+  fonts_unload_custom_font(helvetica_24);
+  fonts_unload_custom_font(helvetica_26);
+
 }
 
 static void init(void) {
--- src/tipcalc.h
+++ src/tipcalc.h
@@ -9,7 +9,7 @@ typedef struct InputField {
     GFont font;
     bool is_selected;
     GRect text_frame;
-    GRect selection_frame;
+    GEdgeInsets selection_insets;
     GetTxtCallback *get_text;
     IncDecCallback *inc_value;
     IncDecCallback *dec_value;
